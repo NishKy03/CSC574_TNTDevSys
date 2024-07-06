adfe914ecb1f98bc0fff97a662be8de26fbd62d7
hugswm <sarahzainab03@gmail.com>
2024-07-07 02:07:21 +0800

    orderStatement done

--- PROJECT TNT/COrderStatement.php
+++ PROJECT TNT/COrderStatement.php
@@ -1,127 +1,6 @@
 <?php
     include('CnavIn.php');
 ?>
-<!--
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Order Statement</title>
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
-        <style>
-            body{
-                margin: 0;
-                padding: 0;
-                height: 100%;
-                font-family: "Poppins", sans-serif;
-            }
-            .progress-container {
-                display: flex;
-                background-color: #4a4a4a;
-                padding: 0;
-                justify-content: center;
-                align-items: center;
-                position: fixed;
-                width: 100%;
-                margin-top: 70px;
-            }
-            .progress-bar {
-                display: flex;
-                justify-content: space-between;
-                align-items: center;
-                width: 50%;
-                background-color: #4a4a4a;
-                padding: 10px 0;
-                position: relative;
-                margin: 0 auto;
-            }
-
-            .step-container {
-                display: flex;
-                flex-direction: column;
-                align-items: center;
-                position: relative;
-            }
-
-            .step {
-                width: 30px;
-                height: 30px;
-                border-radius: 50%;
-                background-color: #777;
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                color: white;
-                font-weight: bold;
-                font-size: 14px;
-                border: 2px solid #4a4a4a;
-                z-index: 1;
-            }
-
-            .step.active {
-                background-color: #f7931e;
-                border-color: #f7931e;
-            }
-
-            .label {
-                margin-top: 5px;
-                color: white;
-                font-size: 14px;
-                text-align: center;
-            }
-
-            .progress-bar::before {
-                content: '';
-                position: absolute;
-                margin-left: 1%;
-                width: 94%;
-                top: 35%;
-                left: 0;
-                right: 0;
-                height: 2px;
-                background-color: #777;
-                z-index: 0;
-            }
-
-            .statement{
-                padding-top: 15%;
-                padding-right:30%;
-                padding-left:auto;
-            }
-
-            img{
-                height: 50px;
-                width: auto;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="progress-container">
-            <div class="progress-bar">
-                <div class="step-container">
-                    <div class="step">1</div>
-                    <div class="label">Order</div>
-                </div>
-                <div class="step-container">
-                    <div class="step">2</div>
-                    <div class="label">Payment</div>
-                </div>
-                <div class="step-container">
-                    <div class="step active">3</div>
-                    <div class="label">Statement</div>
-                </div>
-            </div>
-        </div>
-        <div class="statement">
-            <table>
-                <tr>
-                    <td><img src="images/tntlogo.png"></td>
-                    <td colspan="3">5000001</td>
-                </tr>
-            </table>
-        </div>
-    </body>
-</html>-->
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -299,33 +178,36 @@
     </div>
 </div>
 <div>
+<?php
+    require_once('orderStatementDetails.php');
+?>
 <div class="container">
-    <div id="statement" class="section">
+    <div class="section">
         <table>
             <tr>
                 <td class="right-b"><img src="images/tntlogo.png"></td>
-                <td class="left-b">JHR2024000123</td>
+                <td class="left-b"><h2><?php echo $orderID; ?></h2></td>
             </tr>
             <tr>
-                <td class="bottom-b">To</td>
-                <td class="all-b" rowspan="2" style="word-wrap: break-word; text-align: left;">Abdullah&nbsp;&nbsp;010-2345678&nbsp;&nbsp;No. 12, Taman Juara, 79100 Nusajaya, Johor</td>
+                <td class="bottom-b" style="font-weight:bold; font-size:large;">TO</td>
+                <td class="all-b" rowspan="2" style="word-wrap: break-word; text-align: left;"><?php echo $rName; ?>&nbsp;&nbsp;<?php echo $rPhoneNo; ?>&nbsp;&nbsp;<?php echo $rAddress; ?>,&nbsp;<?php echo $rPostcode; ?>&nbsp;<?php echo $rCity; ?>,&nbsp;<?php echo $rState; ?></td>
             </tr>
-            <tr><td class="top-b">79100</td></tr>
+            <tr><td class="top-b"><?php echo $rPostcode; ?></td></tr>
             <tr>
-                <td class="bottom-b">From</td>
-                <td class="all-b" rowspan="2" style="word-wrap: break-word; text-align: left;">Abdullah&nbsp;&nbsp;010-2345678&nbsp;&nbsp;No. 12, Taman Juara, 79100 Nusajaya, Johor</td>
+                <td class="bottom-b" style="font-weight:bold; font-size:large;">FROM</td>
+                <td class="all-b" rowspan="2" style="word-wrap: break-word; text-align: left;"><?php echo $sName; ?>&nbsp;&nbsp;<?php echo $sPhoneNo; ?>&nbsp;&nbsp;<?php echo $sAddress; ?>,&nbsp;<?php echo $sPostcode; ?>&nbsp;<?php echo $sCity; ?>,&nbsp;<?php echo $sState; ?></td>
             </tr>
-            <tr><td class="top-b">79100</td></tr>
+            <tr><td class="top-b"><?php echo $sPostcode; ?></td></tr>
             <tr>
-                <td class="all-b">14-04-2024</td>
-                <td class="all-b" rowspan="2" style="word-wrap: break-word; text-align: left;">Total = RM 5.90</td>
+                <td class="all-b"><?php echo $orderDate; ?></td>
+                <td class="all-b" rowspan="2" style="word-wrap: break-word; text-align: left;">Total = RM <?php echo $amount; ?></td>
             </tr>
-            <tr><td class="all-b">1.0 KG</td></tr>
+            <tr><td class="all-b"><?php echo $weight; ?> KG</td></tr>
         </table>
     </div>
 
     <div class="actions">
-        <button onclick="printPage()">Print</button>
+        <a href='printOrderStatement.php' target="popup" onclick="window.open('printOrderStatement.php', '_blank', 'width=600,height=400'); return false;">Print</a>
         <button onclick="generateWaybill()">Generate Waybill</button>
     </div>
 </div>
---
+++ PROJECT TNT/orderStatementDetails.php
@@ -0,0 +1,35 @@
+<?php
+    require_once "dbConfig.php";
+    $sql = "SELECT o.orderID, r.name AS rName, r.phoneNo AS rPhoneNo, r.addressLine1 AS rAddress, r.postcode AS rPostcode, r.city AS rCity, r.state AS rState, s.senderName, s.senderPhoneNo, s.addressLine1 AS sAddress, s.postcode As sPostcode, s.city AS sCity, s.state AS sState, o.orderDate, o.parcelWeight, o.totalAmount
+            FROM orders o, recipient r, sender s
+            WHERE o.senderID = s.senderID
+            AND o.recipientID = r.recipientID
+            ORDER BY o.orderID DESC LIMIT 1";
+    
+    if ($result = mysqli_query($conn, $sql)) {
+        if (mysqli_num_rows($result) == 1) {
+            /* Fetch result row as an associative array. Since the result set
+            contains only one row, we don't need to use while loop */
+            $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+            // Retrieve individual field value
+            $orderID = $row['orderID'];
+            $rName = $row['rName'];
+            $rPhoneNo = $row['rPhoneNo'];
+            $rAddress = $row['rAddress'];
+            $rPostcode = $row['rPostcode'];
+            $rCity = $row['rCity'];
+            $rState = $row['rState'];
+            $sName = $row['senderName'];
+            $sPhoneNo = $row['senderPhoneNo'];
+            $sAddress = $row['sAddress'];
+            $sPostcode = $row['sPostcode'];
+            $sCity = $row['sCity'];
+            $sState = $row['sState'];
+            $orderDate = $row['orderDate'];
+            $weight = $row['parcelWeight'];
+            $amount = $row['totalAmount'];
+        } else {
+            exit();
+        }
+    }
